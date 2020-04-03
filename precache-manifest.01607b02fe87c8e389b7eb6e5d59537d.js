a660657c69dd68fc1fdfa4833e4f7fbf26ba3e8b
Isabel Uriarte <uriarte.isabel@gmail.com>
2020-04-02 20:28:38 -0700

    Updates

---
+++ precache-manifest.01607b02fe87c8e389b7eb6e5d59537d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635a459971a6cbd017999128b691f296",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "88c770de23ccb178fa39",
+    "url": "/react-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d79a43f8c53b87465d44",
+    "url": "/react-portfolio/static/js/2.ee497562.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-portfolio/static/js/2.ee497562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c770de23ccb178fa39",
+    "url": "/react-portfolio/static/js/main.4fa65b58.chunk.js"
+  },
+  {
+    "revision": "6c1a97d7eedcd4b749e8",
+    "url": "/react-portfolio/static/js/runtime-main.fcfd7c4a.js"
+  }
+]);
